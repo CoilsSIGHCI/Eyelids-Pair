5f46a75be0baffaf313ee42fe36e9a87f345fd06
Yanbo Wong <40051361+StevenRCE0@users.noreply.github.com>
2024-03-25 18:11:09 +0800

    Initial Commit

---
+++ EyelidsPair/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  EyelidsPair
+//
+//  Created by 砚渤 on 2024/3/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
